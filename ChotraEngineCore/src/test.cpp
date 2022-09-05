cfdf569c55dbf30d1a6c34162f44ae50cc6c974d
Uzunig <uzunig@gmail.com>
2022-09-05 08:39:59 +0200

    library test added

---
+++ ChotraEngineCore/src/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "ChotraEngineCore/test.h"
+
+namespace ChotraEngine {
+	void sayHello() {
+		std::cout << "Hello from Chotra Engine Core!" << std::endl;
+	}
+}
---
+++ ChotraEngineEditor/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "ChotraEngineCore/test.h"
+
+int main() {
+	std::cout << "Hello from Chotra Engine Editor!" << std::endl;
+	ChotraEngine::sayHello();
+
+
+	std::cin.get();
+
+}
