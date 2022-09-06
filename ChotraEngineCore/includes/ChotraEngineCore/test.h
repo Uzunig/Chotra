9c875081b11eddb2bc08031376de4ee084694f26
Uzunig <uzunig@gmail.com>
2022-09-06 11:07:34 +0200

    some changes

--- ChotraEngineCore/includes/ChotraEngineCore/test.h
+++
@@ -1,4 +0,0 @@
-
-namespace ChotraEngine {
-	void sayHello();
-}
--- ChotraEngineCore/src/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "ChotraEngineCore/test.h"
-
-namespace ChotraEngine {
-	void sayHello() {
-		std::cout << "Hello from Chotra Engine Core!" << std::endl;
-	}
-}
