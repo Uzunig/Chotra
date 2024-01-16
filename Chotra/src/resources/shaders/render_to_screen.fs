501afcd8307e6975e97c6ad10c85a82221d9fa6f
Uzunig <Uzunig@gmail.com>
2024-01-16 08:42:54 +0100

    refactoring geometry pass

---
+++ Chotra/src/resources/shaders/render_to_screen.fs
@@ -0,0 +1,24 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D screenTexture;
+
+uniform float contrast;
+uniform float brightness;
+
+uniform float gamma;
+
+void main()
+{             
+    vec3 color = texture(screenTexture, TexCoords).rgb; 
+   
+    color = pow(color + vec3(brightness), vec3(1.0 + contrast));
+
+    // Gamma-correction       
+    color = pow(color, vec3(1.0 / gamma));
+
+    FragColor = vec4(color , 1.0);
+
+}
